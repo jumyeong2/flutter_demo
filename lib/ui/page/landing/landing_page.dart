db7ba1314d8fbedf68ac2ab703520ccc18f8a465
jm <joy020926@gmail.com>
2025-12-11 17:56:33 +0900

    됐다!!!스크롤

--- lib/ui/page/landing/landing_page.dart
+++ lib/ui/page/landing/landing_page.dart
@@ -24,6 +24,7 @@ class LandingPage extends StatelessWidget {
         children: [
           // Main Scroll View
           SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
